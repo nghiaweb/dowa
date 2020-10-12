43cb5faa5b06e552f50f5a772a914705a1832a80
unknown <nghiaweb@gmail.com>
2020-10-12 13:53:47 +0700

    create file index php

---
+++ saiyou/special/voice/index.php
@@ -0,0 +1,77 @@
+<!doctype html>
+<html>
+
+<head>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <?php include('../../include/head.php'); ?>
+
+  <meta property="og:title" content="DOWAホールディングス｜採用サイト">
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="">
+  <meta property="og:image" content="">
+  <meta property="og:site_name" content="DOWAホールディングス｜採用サイト">
+  <meta property="og:description" content="">
+  <meta itemprop="image" content="">
+  <title>SPECIAL｜DOWAホールディングス｜採用サイト</title>
+
+  <link href="../../css/common_low.css" rel="stylesheet">
+  <link href="../../css/report.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+  <div id="wrapper">
+    <?php include('../../include/header.php'); ?>
+    <div id="pankuzu">
+    	<div class="container">
+			<ul>
+				<li><a href="/saiyou/">TOP</a></li>
+				<li>パンくず</li>
+				<li>Voice</li>
+			</ul>
+    	</div><!--container-->
+    </div><!--pankuzu-->
+    <div id="pagetitle">
+    	<div class="ttl-wrap">
+		 <h2 data-aos="fade-up">
+			<span class="en">Special</span>
+			<img src="../../images/voice/img_ttl_voice.png" class="" alt="Special">
+		 </h2>
+   		　<p data-aos="fade-up">
+			 内定者が考えた、あったらいいな採用ページ①<br>
+             内定者の声<br>
+             就活プレイバック　本音でぶっちゃけます、就職<br>
+             活動でのアレコレ。 
+		  </p>
+    	</div>
+     	<div class="mv-img">
+      		<img src="../../images/voice/img_mv.png">
+      	</div>
+      	
+      	<span class="page-en-ttl en" data-aos="fade-right">
+      		Special
+      	</span>
+    </div><!--pagetitle-->
+    
+    
+    <main>
+      <section id="report-content" >
+        <div class="container">
+  		    Content
+        </div><!--container-->
+      </section><!--faq-content-->
+      
+    
+
+    </main>
+    
+    
+  
+	
+   <?php include('../../include/footer.php'); ?>
+  </div>
+  <!--wrapper-->
+
+
+</body>
+
+</html>
